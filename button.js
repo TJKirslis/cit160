efb6342ddf6a542385b1bc4c37a8c3b3aea7ac9a
TJKirslis <kir19001@byui.edu>
2022-01-17 20:37:33 -0700

    yes

---
+++ button.js
@@ -0,0 +1,7 @@
+let Discount=0
+Discount=document.querySelector("#Discount");
+let totalAmountDue=100;
+totalAmountDue=100-Discount;
+
+
+document.querySelector("#Total_Amount_Due").textContent=`Total Amount Due:$ ${totalAmountDue}`;
