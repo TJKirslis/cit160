f091aba0fea35a8bf2110d560afbdc3ce36627e9
TJKirslis <kir19001@byui.edu>
2022-01-29 18:26:48 -0700

    Week4 work

---
+++ week4assignment/strings.js
@@ -0,0 +1,11 @@
+document.querySelector('#copyright').textContent = new Date().getFullYear();
+
+function ___() {
+	// INPUT
+	
+	// PROCESSING
+	 
+	
+	//OUTPUT - Example output: "And it came to pass that the people of ___ did till the land, and raise all manner of ___, and flocks of herds, and flocks of all manner of ___ of every kind, and ___."
+	 
+}
